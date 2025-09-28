ece1495248eda07744cae547ef49d68ce5f8c84e
oshadha thinura <oshadhathinurakithmina@gmail.com>
2025-09-28 17:49:38 +0530

    Delete ddl_Book.sql

--- ddl_Book.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE book
-(
-    id       BIGINT AUTO_INCREMENT NOT NULL,
-    title    VARCHAR(255)          NULL,
-    author   VARCHAR(255)          NULL,
-    price    DOUBLE                NOT NULL,
-    quantity INT                   NOT NULL,
-    CONSTRAINT pk_book PRIMARY KEY (id)
-);
